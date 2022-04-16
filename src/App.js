ce2746ac04d5a1f80fa1093e57cacdec9c5b309b
Ali <ar.k@outlook.com>
2022-04-16 11:19:06 -0700

    create all needed components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import Home from './components/Home';
+import Cart from './components/Cart';
 
 function App() {
   return (
     <div>
-      
+      <Home />
+      <Cart />
     </div>
   );
 }
